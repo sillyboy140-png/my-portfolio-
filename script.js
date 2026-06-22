3e014740d56c4a4b0b9dab4ce010e75510b3ab8e
sillyboy140-png <sillyboy140@gmail.com>
2026-06-22 17:26:13 +0700

    third commit

--- script.js
+++ script.js
@@ -5,28 +5,20 @@ menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
-// Đóng menu khi click vào link
-document.querySelectorAll(".nav-links a").forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
-    });
+// BACK TO TOP
+const btn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 200){
+        btn.classList.add("show");
+    }else{
+        btn.classList.remove("show");
+    }
 });
 
-// Hiệu ứng xuất hiện khi cuộn
-const sections = document.querySelectorAll(".section");
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.style.opacity = "1";
-            entry.target.style.transform = "translateY(0)";
-        }
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
     });
-});
-
-sections.forEach(section => {
-    section.style.opacity = "0";
-    section.style.transform = "translateY(50px)";
-    section.style.transition = "all 0.6s ease";
-    observer.observe(section);
 });
